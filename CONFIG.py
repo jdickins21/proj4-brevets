dbdde401bb9cf5deabfa6feeadbcf7517fd8d8e8
Jacob <jdickins@uoregon.edu>
2016-10-23 15:40:28 -0700

    update requirements

--- CONFIG.py
+++ CONFIG.py
@@ -1,13 +1,13 @@
 """
 Configuration of vocabulary game server.
-Generated Mon Oct 17 17:59:31 PDT 2016
+Generated Sun Oct 23 15:39:36 PDT 2016
 Edit to fit development or deployment environment.
 
 """
 
 PORT=5000
 DEBUG = True  # Set to False for production use
-secret_key="cc1b4cfc9d23b3b0851a19e8606c5679"
+secret_key="6513ab6b3e651ea7d828612433a8a169"
 success_at_count = 3  # How many matches before we declare victory? 
 vocab="data/vocab.txt"  # CHANGE THIS to use another vocabulary file
 
