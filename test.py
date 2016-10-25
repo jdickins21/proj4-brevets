edd25d9edf45d991f29f81a99ccfce5f630afcd1
Jacob <jdickins@uoregon.edu>
2016-10-24 19:05:48 -0700

    finished testing

--- test.py
+++ test.py
@@ -8,99 +8,51 @@ NOW = arrow.Arrow(2016, 5, 11, 8, 0)
 
 def test_one():
 	val = acp_times.open_time(0, 200, NOW.isoformat())
-	if val == NOW.isoformat():
-		print("true")
-	else:
-		print("now: " + NOW.isoformat())
-		print("got: " + val)
+	assert val == NOW.isoformat()
 
 def test_two():
 	val = acp_times.close_time(0, 200, NOW.isoformat())
-	if val == NOW.replace(hours = 1).isoformat():
-		print("true")
-	else:
-		print("now: " + NOW.isoformat())
-		print("got: " + val)
+	assert val == NOW.replace(hours = 1).isoformat()
 
 def test_three():
 	val = acp_times.open_time(50, 200, NOW.isoformat())
-	if val == NOW.replace(hours = 1, minutes = 28).isoformat():
-		print("true")
-	else:
-		print("now: " + NOW.replace(hours = 1, minutes = 28).isoformat())
-		print("got: " + val)
+	assert val == NOW.replace(hours = 1, minutes = 28).isoformat()
 
 def test_four():
 	val = acp_times.close_time(50, 200, NOW.isoformat())
-	if val == NOW.replace(hours = 3, minutes = 21).isoformat():
-		print("true")
-	else:
-		print("now: " + NOW.replace(hours = 3, minutes = 21).isoformat())
-		print("got: " + val)
+	assert val == NOW.replace(hours = 3, minutes = 21).isoformat()
 
 def test_five():
 	val = acp_times.open_time(200, 200, NOW.isoformat())
-	if val == NOW.replace(hours = 5, minutes = 52).isoformat():
-		print("true")
-	else:
-		print("now: " + NOW.replace(hours = 5, minutes = 52).isoformat())
-		print("got: " + val)
+	assert val == NOW.replace(hours = 5, minutes = 52).isoformat()
 
 def test_six():
 	val = acp_times.close_time(200, 200, NOW.isoformat())
-	if val == NOW.replace(hours = 13, minutes = 30).isoformat():
-		print("true")
-	else:
-		print("now: " + NOW.replace(hours = 13, minutes = 30).isoformat())
-		print("got: " + val)
+	assert val == NOW.replace(hours = 13, minutes = 30).isoformat()
 
 def test_seven():
 	val = acp_times.open_time(210, 200, NOW.isoformat())
-	if val == NOW.replace(hours = 5, minutes = 52).isoformat():
-		print("true")
-	else:
-		print("now: " + NOW.replace(hours = 5, minutes = 52).isoformat())
-		print("got: " + val)
+	assert val == NOW.replace(hours = 5, minutes = 52).isoformat()
 
 def test_eight():
 	val = acp_times.close_time(210, 200, NOW.isoformat())
-	if val == NOW.replace(hours = 13, minutes = 30).isoformat():
-		print("true")
-	else:
-		print("now: " + NOW.replace(hours = 13, minutes = 30).isoformat())
-		print("got: " + val)
+	assert val == NOW.replace(hours = 13, minutes = 30).isoformat()
 
 def test_nine():
 	val = acp_times.open_time(300, 1000, NOW.isoformat())
-	if val == NOW.replace(hours = 9).isoformat():
-		print("true")
-	else:
-		print("now: " + NOW.replace(hours = 9).isoformat())
-		print("got: " + val)
+	assert val == NOW.replace(hours = 9).isoformat()
 
 def test_ten():
 	val = acp_times.close_time(300, 1000, NOW.isoformat())
-	if val == NOW.replace(hours = 20).isoformat():
-		print("true")
-	else:
-		print("now: " + NOW.replace(hours = 20).isoformat())
-		print("got: " + val)
+	assert val == NOW.replace(hours = 20).isoformat()
 
 def test_eleven():
 	val = acp_times.open_time(1000, 1000, NOW.isoformat())
-	if val == NOW.replace(hours = 33, minutes = 5).isoformat():
-		print("true")
-	else:
-		print("now: " + NOW.replace(hours = 33, minutes = 5).isoformat())
-		print("got: " + val)
+	assert val == NOW.replace(hours = 33, minutes = 5).isoformat()
 
 def test_twelve():
 	val = acp_times.close_time(1000, 1000, NOW.isoformat())
-	if val == NOW.replace(hours = 75).isoformat():
-		print("true")
-	else:
-		print("now: " + NOW.replace(hours = 75).isoformat())
-		print("got: " + val)
+	assert val == NOW.replace(hours = 75).isoformat()
 
 
 if __name__ == "__main__":
